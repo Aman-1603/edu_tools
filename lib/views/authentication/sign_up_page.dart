c71b7b8c9b8e39d5f9d694326ecf9e00b717af9d
Aman-1603 <59969732+Aman-1603@users.noreply.github.com>
2022-09-25 11:13:37 +0530

    join meeting implimented........

--- lib/views/authentication/sign_up_page.dart
+++ lib/views/authentication/sign_up_page.dart
@@ -1,14 +1,16 @@
 import 'package:edu_tools/Home.dart';
+import 'package:edu_tools/screen/home_lobby_screen.dart';
+import 'package:edu_tools/screen/main_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import '../../app_styles.dart';
 import '../../size_configs.dart';
+import '../../widgets/buttons/custom_button.dart';
 import '../pages.dart';
 import '../../widgets/widgets.dart';
 
 import 'package:edu_tools/resources/auth_methods.dart';
-
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({Key? key}) : super(key: key);
@@ -50,11 +52,9 @@ class _SignUpPageState extends State<SignUpPage> {
 
   //added
 
-
   //for login signup using google
   final AuthMethods _authMethods = AuthMethods();
   //upto this
-
 
   @override
   Widget build(BuildContext context) {
@@ -69,10 +69,8 @@ class _SignUpPageState extends State<SignUpPage> {
             child: Column(
               children: [
                 Container(
-                  child:
-
-                      Image.asset('assets/images/auth/login_image.png'),
-                      width: double.infinity,
+                  child: Image.asset('assets/images/auth/login_image.png'),
+                  width: double.infinity,
                 ),
                 SizedBox(
                   height: height * 2,
@@ -98,12 +96,10 @@ class _SignUpPageState extends State<SignUpPage> {
                       //   validator: nameValidator,
                       // ),
                       TextField(
-
                         onChanged: (value) {
                           email = value;
                         },
                         decoration: InputDecoration(hintText: 'Email'),
-
                       ),
                       TextField(
                         onChanged: (value) {
@@ -111,7 +107,6 @@ class _SignUpPageState extends State<SignUpPage> {
                         },
                         obscureText: true,
                         decoration: InputDecoration(hintText: 'Password'),
-
                       ),
                       MyCheckBox(
                         text: 'Keep me signed in',
@@ -126,40 +121,36 @@ class _SignUpPageState extends State<SignUpPage> {
                             UserCredential userCredential = await FirebaseAuth
                                 .instance
                                 .createUserWithEmailAndPassword(
-                                email: email, password: pass);
-                            Fluttertoast.showToast(msg: "You Have Sucessfully Registred");
+                                    email: email, password: pass);
+                            Fluttertoast.showToast(
+                                msg: "You Have Sucessfully Registred");
 
                             //Adding advance custom dialog
 
-                            showDialog(context: context, builder: (BuildContext context){
-                              return AdvanceCustomAlert();
-                            });
+                            showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AdvanceCustomAlert();
+                                });
 
-                          //Alert Dialog ended here
-
-
+                            //Alert Dialog ended here
 
                             //not navigating here for advance dialogbox
 
-
-                          // Navigator.of(context).pushReplacement(
-                          // MaterialPageRoute(builder: (context) => LoginPage()));
-
-
-
+                            // Navigator.of(context).pushReplacement(
+                            // MaterialPageRoute(builder: (context) => LoginPage()));
 
                           } on FirebaseAuthException catch (e) {
                             if (e.code == 'weak-password') {
                               print('The password provided is too weak.');
                             } else if (e.code == 'email-already-in-use') {
-                              print('The account already exists for that email.');
+                              print(
+                                  'The account already exists for that email.');
                             }
                           } catch (e) {
                             print(e);
                           }
                         },
-
-
                       ),
                     ],
                   ),
@@ -189,9 +180,28 @@ class _SignUpPageState extends State<SignUpPage> {
                 ),
                 Row(
                   children: [
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Expanded(
+                      child: CustomButton(
+                        text: 'Google Sign In',
+                        onPressed: () async {
+                          bool res =
+                              await _authMethods.signInWithGoogle(context);
+                          if (res) {
+                            Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                    builder: (context) => main_screen()));
+                          }
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
                     Expanded(
                       child: LargeIconButton(
-
                         buttonName: 'Google',
                         iconImage: 'assets/images/auth/google_icon.png',
                         onPressed: () {
@@ -206,8 +216,6 @@ class _SignUpPageState extends State<SignUpPage> {
                         //         MaterialPageRoute(builder: (context) => HomeScreen()));
                         //   }
                         // },
-
-
                       ),
                     ),
                     SizedBox(
@@ -247,17 +255,13 @@ class _SignUpPageState extends State<SignUpPage> {
       ),
     );
   }
-  
 }
 
 class AdvanceCustomAlert extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Dialog(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(4)
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
       child: Stack(
         // overflow: overflow.visible,
         clipBehavior: Clip.none,
@@ -266,43 +270,53 @@ class AdvanceCustomAlert extends StatelessWidget {
           Container(
             height: 250,
             child: Padding(
-              padding: const EdgeInsets.fromLTRB(10,60,10,10),
+              padding: const EdgeInsets.fromLTRB(10, 60, 10, 10),
               child: Column(
-
                 children: [
-                  Text('Succesfully Registred', style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),textAlign: TextAlign.center,),
-                  SizedBox(height: 5,),
-                  Text('Now Login to Start Enter Edutools', style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),textAlign: TextAlign.center,),
-                  SizedBox(height: 20,),
-
-                  RaisedButton(onPressed: (){
-
-                    Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(builder: (context) => LoginPage()));
-
-
-                  },
-                  child: Text('Okay',style: TextStyle(color: Colors.white),),
+                  Text(
+                    'Succesfully Registred',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    textAlign: TextAlign.center,
                   ),
-
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    'Now Login to Start Enter Edutools',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (context) => LoginPage()));
+                    },
+                    child: Text(
+                      'Okay',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
                 ],
               ),
             ),
           ),
-
-          Positioned(child: CircleAvatar(
-            backgroundColor: Colors.redAccent,
-            radius: 60,
-            child: Icon(Icons.align_horizontal_right,size: 50,color: Colors.white,),
-          ),
-          top: -70,
+          Positioned(
+            child: CircleAvatar(
+              backgroundColor: Colors.redAccent,
+              radius: 60,
+              child: Icon(
+                Icons.align_horizontal_right,
+                size: 50,
+                color: Colors.white,
+              ),
+            ),
+            top: -70,
           )
-
-
         ],
-
       ),
     );
   }
 }
-
